e746bf33d31de702fce401b8f0902e876d2506a9
Achi00 <toolvlg@gmail.com>
2026-05-16 22:50:40 +0400

    creating connection and channels

--- RabbitMq.Producer/Program.cs
+++ RabbitMq.Producer/Program.cs
@@ -1,6 +1,16 @@
+using RabbitMQ.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+var factory = new ConnectionFactory { HostName = "localhost"};
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "message", durable: true, exclusive: false, autoDelete: false, null);
