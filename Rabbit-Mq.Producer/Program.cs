ae3e60af45c2dd3b890ec57dcb441418b42940d1
Achi00 <toolvlg@gmail.com>
2026-05-25 23:12:11 +0400

    fix argument passing

--- Rabbit-Mq.Producer/Program.cs
+++ Rabbit-Mq.Producer/Program.cs
@@ -6,25 +6,51 @@ using var connection = await factory.CreateConnectionAsync();
 
 using var channel = await connection.CreateChannelAsync();
 
-var dlqArgs = new Dictionary<string, object> 
+var workQueueArgs = new Dictionary<string, object>
 {
-    { "x-dead-letter-exchange", "my-dlx" },
+    { "x-dead-letter-exchange", "dead-letter-exchange" },
     { "x-dead-letter-routing-key", "dead-letter-key" }
 };
 
+
 // create exchange
-await channel.ExchangeDeclareAsync("work-exchange", ExchangeType.Direct, true, false, null);
-// dead letter exchange
-await channel.ExchangeDeclareAsync("dead-letter-exchange", ExchangeType.Fanout, true, false, null);
+await channel.ExchangeDeclareAsync(
+    exchange: "work-exchange",
+    type: ExchangeType.Direct,
+    durable: true);
+
+await channel.ExchangeDeclareAsync(
+    exchange: "dead-letter-exchange",
+    type: ExchangeType.Direct,
+    durable: true);
+
 
 // queues
-await channel.QueueDeclareAsync(queue: "work-queue", durable: true, exclusive: false, autoDelete: false, null);
+await channel.QueueDeclareAsync(
+    queue: "work-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: workQueueArgs);
 // dlx queue
-await channel.QueueDeclareAsync(queue: "dead-letter-queue", durable: true, exclusive: false, autoDelete: false, arguments: dlqArgs);
-
+await channel.QueueDeclareAsync(
+    queue: "dead-letter-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
 
 // bindings
-await channel.QueueBindAsync(queue: "work-queue", exchange: "work-exchange", routingKey: "work", arguments: null);
+await channel.QueueBindAsync(
+    queue: "work-queue",
+    exchange: "work-exchange",
+    routingKey: "work");
+// dlx
+
+await channel.QueueBindAsync(
+    queue: "dead-letter-queue",
+    exchange: "dead-letter-exchange",
+    routingKey: "dead-letter-key");
 
 
 for (int i = 1; i <= 20; i++)
