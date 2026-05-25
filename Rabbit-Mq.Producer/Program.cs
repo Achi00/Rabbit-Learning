550839e9c0dac134fd25d57afbb400311cc88667
Achi00 <toolvlg@gmail.com>
2026-05-25 23:07:05 +0400

    create dlx and dlq

--- Rabbit-Mq.Producer/Program.cs
+++ Rabbit-Mq.Producer/Program.cs
@@ -6,10 +6,22 @@ using var connection = await factory.CreateConnectionAsync();
 
 using var channel = await connection.CreateChannelAsync();
 
+var dlqArgs = new Dictionary<string, object> 
+{
+    { "x-dead-letter-exchange", "my-dlx" },
+    { "x-dead-letter-routing-key", "dead-letter-key" }
+};
+
 // create exchange
 await channel.ExchangeDeclareAsync("work-exchange", ExchangeType.Direct, true, false, null);
+// dead letter exchange
+await channel.ExchangeDeclareAsync("dead-letter-exchange", ExchangeType.Fanout, true, false, null);
+
 // queues
 await channel.QueueDeclareAsync(queue: "work-queue", durable: true, exclusive: false, autoDelete: false, null);
+// dlx queue
+await channel.QueueDeclareAsync(queue: "dead-letter-queue", durable: true, exclusive: false, autoDelete: false, arguments: dlqArgs);
+
 
 // bindings
 await channel.QueueBindAsync(queue: "work-queue", exchange: "work-exchange", routingKey: "work", arguments: null);
