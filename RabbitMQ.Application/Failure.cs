b270fc1cd1a11144de7215d5f1aafc48ff311fd2
Achi00 <toolvlg@gmail.com>
2026-05-30 22:29:50 +0400

    add poison consumer data printing

---
+++ RabbitMQ.Application/Failure.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Application
+{
+    public static class Failure
+    {
+        public static async Task FailedMessages(AsyncEventingBasicConsumer consumer)
+        {
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var headers = ea.BasicProperties.Headers;
+                var body = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                Console.WriteLine("Poison messages");
+
+                Console.WriteLine($"Body: {body}");
+                Console.WriteLine($"Retry count: {headers?["x-retry-count"]}");
+                Console.WriteLine($"Original queue: {headers?["x-original-queue"]}");
+                Console.WriteLine($"Failure reason: {headers?["x-fail-reason"]}");
+                Console.WriteLine($"Failed at: {headers?["x-failed-at"]}");
+                Console.WriteLine();
+            };
+
+            await Task.CompletedTask;
+        }
+    }
+}
