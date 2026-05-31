5dc36a080491d953f2374d9758005f7077ee6c3a
Achi00 <toolvlg@gmail.com>
2026-05-31 22:21:51 +0400

    modify topology setup, declare retry queues based on args

--- RabbitMQ.Application/Infrastructure/TopologySetup.cs
+++ RabbitMQ.Application/Infrastructure/TopologySetup.cs
@@ -3,48 +3,45 @@
 namespace RabbitMQ.Application.Infrastructure
 {
     // change to IHostedService? only will run on statup
-    public static class TopologySetup
+    public class TopologySetup
     {
-        public static async Task SetupTopologyAsync(IChannel channel)
+        private readonly RabbitMqConnectionProvider _provider;
+
+        public TopologySetup(RabbitMqConnectionProvider provider)
         {
-            // delays in ms
-            var delays = new[] { 5_000, 30_000, 300_000 };
-
-            // new retry queue pass delay values, if fail back to dlx
-            foreach (var delay in delays)
-            {
-                var retryArgs = new Dictionary<string, object>
-                {
-                    { "x-message-ttl", delay },
-                    { "x-dead-letter-exchange", "orders.exchange" },
-                    { "x-dead-letter-routing-key", "orders" }
-                };
-
-                await channel.QueueDeclareAsync(
-                    queue: $"orders.retry.{delay / 1000}s",
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: retryArgs!
-                );
-            }
-
-            var dlqArgs = new Dictionary<string, object>
-            {
-                { "x-dead-letter-exchange", "orders.dlx" },
-                { "x-message-ttl", 30000 }
-            };
+            _provider = provider;
+        }
+        public async Task StartAsync(CancellationToken ct)
+        {
+            using var channel = await _provider.Connection.CreateChannelAsync();
 
             await channel.ExchangeDeclareAsync("orders.exchange", ExchangeType.Direct, durable: true);
             await channel.ExchangeDeclareAsync("orders.dlx", ExchangeType.Fanout, durable: true);
 
-            await channel.QueueDeclareAsync("orders.queue", durable: true, exclusive: false, autoDelete: false, arguments: dlqArgs);
+            var queueArgs = new Dictionary<string, object>
+            {
+                { "x-dead-letter-exchange", "orders.dlx" }
+            };
+
+            await channel.QueueDeclareAsync("orders.queue", durable: true, exclusive: false, autoDelete: false, arguments: queueArgs);
             await channel.QueueDeclareAsync("orders.dlq", durable: true, exclusive: false, autoDelete: false);
-            // failer queues go here after retries
             await channel.QueueDeclareAsync("orders.poison", durable: true, exclusive: false, autoDelete: false);
+
+            foreach (var delay in new[] { 5_000, 30_000, 300_000 })
+            {
+                var retryArgs = new Dictionary<string, object>
+            {
+                { "x-message-ttl", delay },
+                { "x-dead-letter-exchange", "orders.exchange" },
+                { "x-dead-letter-routing-key", "orders" }
+            };
+                await channel.QueueDeclareAsync($"orders.retry.{delay / 1000}s", durable: true, exclusive: false, autoDelete: false, arguments: retryArgs);
+            }
 
             await channel.QueueBindAsync("orders.queue", "orders.exchange", routingKey: "orders");
             await channel.QueueBindAsync("orders.dlq", "orders.dlx", routingKey: string.Empty);
         }
+
+        public Task StopAsync(CancellationToken ct) => Task.CompletedTask;
     }
 }
