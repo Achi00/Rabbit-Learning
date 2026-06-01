71437380c33263dc950bf1e2b4ef80eae45bd3ec
Achi00 <toolvlg@gmail.com>
2026-06-01 23:04:47 +0400

    introduce topology IHostedService layer

---
+++ RabbitMQ.Application/TopologyHostedService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Application.Infrastructure;
+
+namespace RabbitMQ.Application
+{
+    public class TopologyHostedService : IHostedService
+    {
+        private readonly TopologySetup _topology;
+        public TopologyHostedService(TopologySetup topology)
+        {
+            _topology = topology;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _topology.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
