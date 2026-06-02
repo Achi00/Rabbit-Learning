babf9cf1cee45baad4a1f45120cf7a827f589596
Achi00 <toolvlg@gmail.com>
2026-06-02 23:13:10 +0400

    added custom exception and order validation

---
+++ RabbitMQ.Application/Exceptions/InvalidOrderException.cs
@@ -0,0 +1,13 @@
+﻿namespace RabbitMQ.Application.Exceptions
+{
+    public class InvalidOrderException : Exception
+    {
+        public InvalidOrderException()
+        {
+        }
+
+        public InvalidOrderException(string? message) : base(message)
+        {
+        }
+    }
+}
--- RabbitMQ.Application/Services/FakeOrderProcessor.cs
+++ RabbitMQ.Application/Services/FakeOrderProcessor.cs
@@ -1,13 +1,24 @@
-﻿using RabbitMQ.Application.Models;
+﻿using RabbitMQ.Application.Exceptions;
+using RabbitMQ.Application.Models;
 using RabbitMQ.Application.Services.Interfaces;
 
 namespace RabbitMQ.Application.Services
 {
     public sealed class FakeOrderProcessor : IOrderProcessor
     {
+        // 70% success 30% failure, to test different acknowledgement patterns
         private readonly Random _random = new Random();
         public async Task ProcessOrderAsync(OrderMessage order)
         {
+            if (string.IsNullOrWhiteSpace(order.CustomerEmail))
+            {
+                throw new InvalidOrderException("Customer email mising");
+            }
+
+            if (order.Amount <= 0)
+            {
+                throw new InvalidOrderException("Order amount must be positive value");
+            }
             await Task.Delay(1000);
 
             var result = _random.Next(1, 10);
