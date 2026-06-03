15e4052e2bff7c764e76b7112c76edccf550748d
Achi00 <toolvlg@gmail.com>
2026-06-03 20:31:30 +0400

    create cts, handle 2 different error scenarios, contains too much abstraction need to dive deeper

--- Rabbit-Mq.Producer/Program.cs
+++ Rabbit-Mq.Producer/Program.cs
@@ -27,9 +27,28 @@ for (int i = 1; i <= 10; i++)
 
     var json = JsonSerializer.Serialize(order);
 
-    await channel.BasicPublishAsync(exchange: "orders.exchange", routingKey: "orders", body: Encoding.UTF8.GetBytes(json));
+    var message = Encoding.UTF8.GetBytes(json);
+
+    using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+
+    try
+    {
+        // after introduced channelOptions in this specs this will wait for broker confirmation before returning
+        await channel.BasicPublishAsync(exchange: "orders.exchange", routingKey: "orders", mandatory: false, body: message, cancellationToken: cts.Token);
+    }
+    catch (OperationCanceledException)
+    {
+        // broker did not confirm within 5 seconds, 5 = cts passed valure
+        // safe to retry publish message may arrive?
+        Console.WriteLine("publisher confirmation timed out, retrying...");
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine(ex.Message, "Publish failed");
+    }
 
     Console.WriteLine($"sent Order with id: {i}");
+    Console.WriteLine("broker confirmed message persisted");
 
     await Task.Delay(2000);
 }
