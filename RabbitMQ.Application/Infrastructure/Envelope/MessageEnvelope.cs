abd42479479813c66198e8c2e6be6ff16c42850a
Achi00 <toolvlg@gmail.com>
2026-06-05 22:03:39 +0400

    introduce envelope pattern to fix hadr coded Deserialized OrderMessage type

---
+++ RabbitMQ.Application/Infrastructure/Envelope/MessageEnvelope.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace RabbitMQ.Application.Infrastructure.Envelope
+{
+    // decide what type name is, will not be depentend on .NET internals and namespaces
+    public record MessageEnvelope
+    {
+        public Guid MessageId { get; init; } = Guid.NewGuid();
+        public string MessageType { get; init; }
+        public JsonElement Payload { get; init; }
+    }
+}
