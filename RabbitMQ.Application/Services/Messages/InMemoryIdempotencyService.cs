96fb921adcde6cba8f5f911d9dc2072747c47f67
Achi00 <toolvlg@gmail.com>
2026-06-07 22:18:41 +0400

    added simple idempotancy version class

---
+++ RabbitMQ.Application/Services/Messages/InMemoryIdempotencyService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+
+namespace RabbitMQ.Application.Services.Messages
+{
+    // simple in memory idempotacny checks
+    public class InMemoryIdempotencyService
+    {
+        private readonly ConcurrentDictionary<Guid, DateTimeOffset> _processed = new();
+
+        public bool IsDuplicate(Guid messageId) => _processed.ContainsKey(messageId);
+
+        public void MarkAsProcessed(Guid messageId) => _processed.TryAdd(messageId, DateTimeOffset.UtcNow);
+    }
+}
