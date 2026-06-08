43be050851174a32c86e17d6a8130421f6b95342
Achi00 <toolvlg@gmail.com>
2026-06-08 23:11:06 +0400

    fix gitignore file

---
+++ Rabbit-Mq.Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Application.Infrastructure;
+using RabbitMQ.Application.Interfaces.Messages;
+using RabbitMQ.Application.Services;
+using RabbitMQ.Application.Services.Interfaces;
+using RabbitMQ.Application.Services.Interfaces.Messages;
+using RabbitMQ.Application.Services.Messages;
+using RabbitMQ.Application.Services.Messages.Orders;
+using RabbitMQ.Application.Workers;
+using RabbitMqDemo.Persistance.Context;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// registed db context
+builder.Services.AddDbContext<MessageDbContext>(options =>
+{
+    // using string for testing purpouse, in future will be use nameof and classes
+    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+        ?? throw new InvalidOperationException("Connection string not found");
+
+    options.UseSqlServer(connectionString);
+});
+
+var connectionProvider = await RabbitMqConnectionProvider.CreateAsync("localhost");
+builder.Services.AddSingleton(connectionProvider);
+
+// Workers
+builder.Services.AddHostedService<TopologySetup>();
+builder.Services.AddHostedService<OrderConsumerWorker>();
+builder.Services.AddHostedService<RetryWorker>();
+builder.Services.AddHostedService<PoisonMessageWorker>();
+builder.Services.AddScoped<IOrderCreateProcessor, FakeOrderProcessor>();
+// Type handlers
+builder.Services.AddKeyedScoped<IMessageHandler, OrderCreatedHandler>("OrderCreated");
+builder.Services.AddKeyedScoped<IMessageHandler, OrderCancelledHandler>("OrderCancelled");
+// Services
+builder.Services.AddScoped<IOrderCreateProcessor, FakeOrderProcessor>();
+builder.Services.AddScoped<IOrderCancelProcessor, FakeOrderCancelProcessor>();
+// Idempotancy service
+builder.Services.AddSingleton<InMemoryIdempotencyService>();
+
+var host = builder.Build();
+
+await host.RunAsync();
