fcc960fcff716e9ecfd2f8d94e2f33fa29f34be6
Achi00 <toolvlg@gmail.com>
2026-06-08 22:25:29 +0400

    set up for db idempotency

---
+++ RabbitMqDemo.Persistance/Context/MessageDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMqDemo.Persistance.Context
+{
+    public class MessageDbContext : DbContext
+    {
+        public MessageDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<> MyProperty { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MessageDbContext).Assembly);
+        }
+    }
+}
