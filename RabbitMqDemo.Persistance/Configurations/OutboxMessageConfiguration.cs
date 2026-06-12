2c09966eb5ada5af377778a762cad7917a92a55e
Achi00 <toolvlg@gmail.com>
2026-06-12 23:11:18 +0400

    introduce outbox message ef config

---
+++ RabbitMqDemo.Persistance/Configurations/OutboxMessageConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RabbitMq.Domain.Entity;
+
+namespace RabbitMqDemo.Persistance.Configurations
+{
+    public class OutboxMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+        {
+            builder.ToTable("OutboxMessages");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.MessageType)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Payload)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.HasIndex(x => x.SentAt);
+        }
+    }
+}
