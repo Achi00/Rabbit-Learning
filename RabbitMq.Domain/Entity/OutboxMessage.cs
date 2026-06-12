bb95c6e54527f93fe0032db81cd3c1a6944e36da
Achi00 <toolvlg@gmail.com>
2026-06-12 23:06:08 +0400

    add outbox pattern domain model

---
+++ RabbitMq.Domain/Entity/OutboxMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace RabbitMq.Domain.Entity
+{
+    public sealed class OutboxMessage
+    {
+        public Guid Id { get; init; }
+        public string MessageType { get; init; }
+        public string Payload { get; init; }
+        public DateTimeOffset CreatedAt { get; init; }
+        public DateTimeOffset? SentAt { get; set; }
+    }
+}
