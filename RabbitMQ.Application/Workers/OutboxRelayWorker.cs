e004dfd35a4d6b3e5aeef0276245b429ef0f518b
Achi00 <toolvlg@gmail.com>
2026-06-13 23:41:35 +0400

    create outbox message worker

---
+++ RabbitMQ.Application/Workers/OutboxRelayWorker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMqDemo.Persistance.Context;
+
+namespace RabbitMQ.Application.Workers
+{
+    public class OutboxRelayWorker : BackgroundService
+    {
+        private readonly IServiceProvider _services;
+        private readonly ILogger<OutboxRelayWorker> _logger;
+
+        public OutboxRelayWorker(IServiceProvider services, ILogger<OutboxRelayWorker> logger)
+        {
+            _services = services;
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //await P
+            }
+        }
+
+        private async Task ProcessPendingMessagesAsync(CancellationToken ct)
+        {
+            await using var scope = _services.CreateAsyncScope();
+            var db = scope.ServiceProvider.GetRequiredService<MessageDbContext>();
+        }
+    }
+}
