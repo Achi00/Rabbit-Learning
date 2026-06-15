821764f9a4951feffa54ac56a875d8251bdb7c97
Achi00 <toolvlg@gmail.com>
2026-06-15 23:39:10 +0400

    saga pattern models setup

---
+++ RabbitMQ.Application/Enums/SagaStep.cs
@@ -0,0 +1,12 @@
+﻿namespace RabbitMQ.Application.Enums
+{
+    public enum SagaStep
+    {
+        Started,
+        StockReserved,
+        PaymentCharged, 
+        Completed, 
+        Compensating,
+        Cancelled
+    }
+}
---
+++ RabbitMQ.Application/Models/SagaState.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Application.Enums;
+
+namespace RabbitMQ.Application.Models
+{
+    public class SagaState
+    {
+        public Guid SagaId { get; set; }
+        public SagaStep CurrentStep { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
