9225fe2dd8fa29a312d5b383ddd443b2e9d853b4
Achi00 <toolvlg@gmail.com>
2026-06-16 23:30:33 +0400

    handle inventory saga operations

--- RabbitMQ.Application/Services/Messages/Inventory/InventoryHandler.cs
+++ RabbitMQ.Application/Services/Messages/Inventory/InventoryHandler.cs
@@ -1,4 +1,7 @@
-﻿using RabbitMQ.Application.Services.Interfaces.Messages;
+﻿using RabbitMq.Contracts.Commands;
+using RabbitMq.Contracts.Events;
+using RabbitMq.Domain.Entity;
+using RabbitMQ.Application.Services.Interfaces.Messages;
 using RabbitMQ.Application.Services.Messages.Idempotency;
 using RabbitMqDemo.Persistance.Context;
 using System.Text.Json;
@@ -18,7 +21,29 @@ namespace RabbitMQ.Application.Services.Messages.Inventory
                 return;
             }
 
-            //var command = payload
+            // commands to controll certain operation, in this case reserving inventory stock if random value is successful
+            var command = payload.Deserialize<ReserveStockCommand>();
+
+            // simulate 70% success rate
+            var success = _random.Next(1, 10) > 3;
+
+            OutboxMessage outboxMessage;
+
+            // saga stock reserved if success = true
+            if (success)
+            {
+                outboxMessage = new OutboxMessage { MessageType = "StockReserved", Payload = JsonSerializer.Serialize(new StockReservedEvent(command!.SagaId, command.OrderId))};
+            }
+            else
+            {
+                outboxMessage = new OutboxMessage { MessageType = "StockReservationFailed", Payload = JsonSerializer.Serialize(new StockReservationFailedEvent(command!.SagaId, command.OrderId, "Out of stock")) };
+            }
+
+            await _context.OutboxMessages.AddAsync(outboxMessage);
+
+            _idempotency.MarkAsProcessed(messageId, "ReserveStock");
+
+            await _context.SaveChangesAsync();
         }
     }
 }
