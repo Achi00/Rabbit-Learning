2b3d84b346b8d6aa0db0aac29eb6ebf08ec8adcb
Achi00 <toolvlg@gmail.com>
2026-06-16 23:58:10 +0400

    add saga coordinator

---
+++ RabbitMQ.Application/Sagas/OrderSagaCoordinator.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMq.Contracts.Commands;
+using RabbitMq.Contracts.Events;
+using RabbitMq.Domain.Entity;
+using RabbitMQ.Application.Enums;
+using RabbitMqDemo.Persistance.Context;
+using System.Text.Json;
+
+namespace RabbitMQ.Application.Sagas
+{
+    public class OrderSagaCoordinator
+    {
+        private readonly MessageDbContext _context;
+
+        public async Task OnStockReservedAsync(StockReservedEvent evt)
+        {
+            var saga = await _context.OrderSagaState.FindAsync(evt.SagaId);
+            saga.CurrentStep = SagaStep.StockReserved;
+            saga.UpdatedAt = DateTimeOffset.UtcNow;
+
+            await _context.OutboxMessages.AddAsync(new OutboxMessage
+            {
+                Id = Guid.NewGuid(),
+                MessageType = "ChargePayment",
+                // dummy data for testing
+                Payload = JsonSerializer.Serialize(new ChargePaymentCommand(evt.SagaId, evt.OrderId, 1000, "Test@email.com")),
+                CreatedAt = DateTimeOffset.UtcNow
+            });
+        }
+
+        public async Task OnPaymentFailedAsync(PaymentFailedEvent evt)
+        {
+            var saga = await _context.OrderSagaState.FindAsync(evt.SagaId);
+            saga.CurrentStep = SagaStep.Compensating;
+            saga.UpdatedAt = DateTimeOffset.UtcNow;
+
+            await _context.OutboxMessages.AddAsync(new OutboxMessage
+            {
+                Id = Guid.NewGuid(),
+                MessageType = "ReleaseStock",
+                Payload = JsonSerializer.Serialize(new ReleaseStockCommand(evt.SagaId, evt.OrderId)),
+                CreatedAt = DateTimeOffset.UtcNow
+            });
+        }
+
+
+        public Task SaveAsync() => _context.SaveChangesAsync();
+    }
+}
---
+++ RabbitMQ.Application/Services/Messages/OrderHandlers/StockReservedHandler.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMq.Contracts.Events;
+using RabbitMQ.Application.Sagas;
+using RabbitMQ.Application.Services.Interfaces.Messages;
+using RabbitMQ.Application.Services.Messages.Idempotency;
+using System.Text.Json;
+
+namespace RabbitMQ.Application.Services.Messages.OrderHandlers
+{
+    public class StockReservedHandler : IMessageHandler
+    {
+        private readonly OrderSagaCoordinator _coordinator;
+        private readonly DbIdempotencyService _idempotency;
+
+        public async Task HandleAsync(JsonElement payload, Guid messageId)
+        {
+            if (await _idempotency.IsDuplicateAsync(messageId)) return;
+
+            var evt = payload.Deserialize<StockReservedEvent>()!;
+            await _coordinator.OnStockReservedAsync(evt);
+
+            _idempotency.MarkAsProcessed(messageId, "StockReserved");
+            await _coordinator.SaveAsync();
+        }
+    }
+}
