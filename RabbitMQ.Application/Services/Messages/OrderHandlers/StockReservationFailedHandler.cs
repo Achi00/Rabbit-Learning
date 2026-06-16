6e19303f0c2705ae1d09c616705b0fc36f00cf89
Achi00 <toolvlg@gmail.com>
2026-06-16 23:59:32 +0400

    add missed hadler

---
+++ RabbitMQ.Application/Services/Messages/OrderHandlers/StockReservationFailedHandler.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Application.Sagas;
+using RabbitMQ.Application.Services.Interfaces.Messages;
+using RabbitMQ.Application.Services.Messages.Idempotency;
+using System.Text.Json;
+
+namespace RabbitMQ.Application.Services.Messages.OrderHandlers
+{
+    public class StockReservationFailedHandler : IMessageHandler
+    {
+        private readonly OrderSagaCoordinator _coordinator;
+        private readonly DbIdempotencyService _idempotency;
+
+        public Task HandleAsync(JsonElement payload, Guid messageId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
