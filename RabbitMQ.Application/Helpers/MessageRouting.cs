5d319603d84e531aac30036d550e7fe3459c9e9b
Achi00 <toolvlg@gmail.com>
2026-06-18 23:56:20 +0400

    add all missing entries to MessageRouting

--- RabbitMQ.Application/Helpers/MessageRouting.cs
+++ RabbitMQ.Application/Helpers/MessageRouting.cs
@@ -1,4 +1,6 @@
-﻿namespace RabbitMQ.Application.Helpers
+﻿using RabbitMq.Contracts;
+
+namespace RabbitMQ.Application.Helpers
 {
     public static class MessageRouting
     {
@@ -6,8 +8,16 @@
         // adding one line here will work in RabbitMqPublisher
         private static readonly Dictionary<string, (string Exchange, string RoutingKey)> _routes = new()
         {
-            ["OrderCreated"] = ("orders.exchange", "order.created"),
-            ["OrderCancelled"] = ("orders.exchange", "order.cancelled"),
+            [MessageTypes.OrderCreated] = ("orders.exchange", "order.created"),
+            [MessageTypes.OrderCancelled] = ("orders.exchange", "order.cancelled"),
+            [MessageTypes.ReserveStock] = ("orders.exchange", "saga.reserve-stock"),
+            [MessageTypes.StockReserved] = ("orders.exchange", "saga.stock-reserved"),
+            [MessageTypes.StockReservationFailed] = ("orders.exchange", "saga.stock-reservation-failed"),
+            [MessageTypes.ChargePayment] = ("orders.exchange", "saga.charge-payment"),
+            [MessageTypes.PaymentCharged] = ("orders.exchange", "saga.payment-charged"),
+            [MessageTypes.PaymentFailed] = ("orders.exchange", "saga.payment-failed"),
+            [MessageTypes.ReleaseStock] = ("orders.exchange", "saga.release-stock"),
+            [MessageTypes.StockReleased] = ("orders.exchange", "saga.stock-released")
         };
 
         public static (string Exchange, string RoutingKey) Resolve(string messageType)
