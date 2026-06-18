03ff0ea3a1a45a1daf609e9bf912fd87516a7414
Achi00 <toolvlg@gmail.com>
2026-06-18 14:30:27 +0400

    add oayment handler with randomized success rate for testing

---
+++ RabbitMQ.Application/Services/Orders/OrderHandlers/PaymentHandler.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMq.Contracts;
+using RabbitMq.Contracts.Commands;
+using RabbitMq.Contracts.Events;
+using RabbitMq.Domain.Entity;
+using RabbitMQ.Application.Services.Interfaces.Messages;
+using RabbitMQ.Application.Services.Messages.Idempotency;
+using RabbitMqDemo.Persistance.Context;
+using System.Text.Json;
+
+namespace RabbitMQ.Application.Services.Orders.OrderHandlers
+{
+    public class PaymentHandler : IMessageHandler
+    {
+        private readonly Random _random = new Random();
+        private readonly MessageDbContext _context;
+        private readonly DbIdempotencyService _idempotency;
+
+        public PaymentHandler(MessageDbContext context, DbIdempotencyService idempotency)
+        {
+            _context = context;
+            _idempotency = idempotency;
+        }
+
+        public async Task HandleAsync(JsonElement payload, Guid messageId)
+        {
+            // idempotency check, see if message id was already seen
+            if (await _idempotency.IsDuplicateAsync(messageId))
+            {
+                return;
+            }
+
+            var command = payload.Deserialize<ChargePaymentCommand>();
+
+            // randomize success by 70%
+            var success = _random.Next(1, 10) > 3;
+
+            var outboxMessage = success
+                ? new OutboxMessage
+                {
+                    Id = Guid.NewGuid(),
+                    MessageType = MessageTypes.PaymentCharged,
+                    Payload = JsonSerializer.Serialize(new PaymentChargedEvent(command.SagaId, command.OrderId))
+                }
+                : new OutboxMessage
+                {
+                    Id = Guid.NewGuid(),
+                    MessageType = MessageTypes.PaymentFailed,
+                    Payload = JsonSerializer.Serialize(new PaymentFailedEvent(command.SagaId, command.OrderId, "Card Declined"))
+                };
+
+            await _context.OutboxMessages.AddAsync(outboxMessage);
+            _idempotency.MarkAsProcessed(messageId, MessageTypes.ChargePayment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
