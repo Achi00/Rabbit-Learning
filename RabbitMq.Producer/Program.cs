edc770da4e5727eed02eeeb5da12262c0edcef79
Achi00 <toolvlg@gmail.com>
2026-06-22 22:20:16 +0400

    setup masstransit, rabbitmq connection and ef in producer web api

--- RabbitMq.Producer/Program.cs
+++ RabbitMq.Producer/Program.cs
@@ -1,3 +1,6 @@
+using MassTransit;
+using RabbitMqDemo.Persistance.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// masstransit
+builder.Services.AddMassTransit(x => 
+{
+    x.AddEntityFrameworkOutbox<MessageDbContext>(c =>
+    {
+        c.UseSqlServer();
+        c.UseBusOutbox();
+    });
+
+    x.UsingRabbitMq((ctx, cfg) => 
+    {
+        cfg.Host("localhost");
+        // replaces my prev TopologySetup class
+        cfg.ConfigureEndpoints(ctx);
+    });
+});
 
 var app = builder.Build();
 
