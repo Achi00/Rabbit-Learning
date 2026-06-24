919b9810870e1e24d21c42f6f80a857191c79255
Achi00 <toolvlg@gmail.com>
2026-06-24 22:33:50 +0400

    introduce new ReserveStockConsumer

---
+++ RabbitMq.Infrastructure/Messaging/Consumers/ReserveStockConsumer.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using RabbitMq.Contracts.Commands;
+using RabbitMq.Contracts.Events;
+
+namespace RabbitMq.Infrastructure.Messaging.Consumers
+{
+    public class ReserveStockConsumer : IConsumer<ReserveStock>
+    {
+        public async Task Consume(ConsumeContext<ReserveStock> context)
+        {
+            // randomize succes for testing
+            var success = Random.Shared.Next(10) > 3;
+
+            if (success)
+            {
+                await context.Publish
+                (
+                    new StockReserved(context.Message.SagaId, context.Message.OrderId)
+                );
+            }
+            else
+            {
+                await context.Publish
+                (
+                    new StockReservationFailed(context.Message.SagaId, context.Message.OrderId, "Out of stock")
+                );
+            }
+        }
+    }
+}
