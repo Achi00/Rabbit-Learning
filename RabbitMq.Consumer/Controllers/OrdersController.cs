f5d3f78394b539c347a7f814660e3a8a02d37389
Achi00 <toolvlg@gmail.com>
2026-06-26 00:00:09 +0400

    add order controller

--- RabbitMq.Consumer/Controllers/OrdersController.cs
+++ RabbitMq.Consumer/Controllers/OrdersController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMq.Contracts.Events;
+using RabbitMq.Domain.Entity;
+using RabbitMqDemo.Persistance.Context;
 
 namespace RabbitMq.Api.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class OrdersController : ControllerBase
     {
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly MessageDbContext _db;
+
+        public OrdersController(IPublishEndpoint publishEndpoint, MessageDbContext db)
+        {
+            _publishEndpoint = publishEndpoint;
+            _db = db;
+        }
+
+        
     }
 }
