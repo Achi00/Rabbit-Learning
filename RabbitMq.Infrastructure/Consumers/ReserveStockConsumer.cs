2c5fd961d8f286fbf8476cc9c0eeb83f051661de
Achi00 <toolvlg@gmail.com>
2026-06-26 23:55:06 +0400

    finished ReserveStockConsumer

--- RabbitMq.Infrastructure/Consumers/ReserveStockConsumer.cs
+++ RabbitMq.Infrastructure/Consumers/ReserveStockConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using MassTransit;
 using RabbitMq.Contracts.Commands;
 using RabbitMq.Contracts.Events;
+using RabbitMQ.Application.Interfaces.Services.Inventory;
 
 namespace RabbitMq.Infrastructure.Messaging.Consumers
 {
@@ -8,9 +9,26 @@ namespace RabbitMq.Infrastructure.Messaging.Consumers
 
     public class ReserveStockConsumer : IConsumer<ReserveStock>
     {
+        private readonly IInventoryService _inventoryService;
+
+        public ReserveStockConsumer(IInventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
         public async Task Consume(ConsumeContext<ReserveStock> context)
         {
-            
+            var message = context.Message;
+            // consumer gets only existing message
+            var result = await _inventoryService.ReserveStockAsync(message.OrderId, context.CancellationToken);
+
+            if (result.Success)
+            {
+                await context.Publish(new StockReserved(message.SagaId, message.OrderId));
+            }
+            else
+            {
+                await context.Publish(new StockReservationFailed(message.SagaId, message.OrderId, result.ErrorMessage ?? "Stock reservation failed"));
+            }
         }
     }
 }
