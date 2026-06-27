11a5eccad49e9a4d2111c6ccfec061a3846bd5be
Achi00 <toolvlg@gmail.com>
2026-06-27 22:44:03 +0400

    add stock release failure event create ReleaseStockConsumer

---
+++ RabbitMq.Contracts/Events/StockReleaseFailed.cs
@@ -0,0 +1,4 @@
+﻿namespace RabbitMq.Contracts.Events
+{
+    public record StockReleaseFailed(Guid SagaId, Guid OrderId, string Reason);
+}
--- RabbitMq.Infrastructure/Consumers/ReleaseStockConsumer.cs
+++ RabbitMq.Infrastructure/Consumers/ReleaseStockConsumer.cs
@@ -1,14 +1,29 @@
 ﻿using MassTransit;
 using RabbitMq.Contracts.Commands;
 using RabbitMq.Contracts.Events;
+using RabbitMQ.Application.Interfaces.Services.Inventory;
 
 namespace RabbitMq.Infrastructure.Messaging.Consumers
 {
     public class ReleaseStockConsumer : IConsumer<ReleaseStock>
     {
+        private readonly IInventoryService _inventoryService;
+        public ReleaseStockConsumer(IInventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
         public async Task Consume(ConsumeContext<ReleaseStock> context)
         {
-            await context.Publish(new StockReleased(context.Message.SagaId, context.Message.OrderId));
+            var result = await _inventoryService.ReleaseStockAsync(context.Message.OrderId);
+
+            if (result.Success)
+            {
+                await context.Publish(new StockReleased(context.Message.SagaId, context.Message.OrderId));
+            }
+            else
+            {
+
+            }
         }
     }
 }
