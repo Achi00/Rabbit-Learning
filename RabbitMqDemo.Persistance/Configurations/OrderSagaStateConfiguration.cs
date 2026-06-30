06b0c1ffffbb6ef126dfefa82e9f9b7cf31a1d46
Achi00 <toolvlg@gmail.com>
2026-06-30 19:09:37 +0400

    moved ef core configuration back to persistance, moved state model in domain layer

---
+++ RabbitMqDemo.Persistance/Configurations/OrderSagaStateConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RabbitMq.Infrastructure.Messaging.Saga;
+
+namespace RabbitMqDemo.Persistance.Configurations
+{
+    public class OrderSagaStateConfiguration : IEntityTypeConfiguration<OrderSagaState>
+    {
+        public void Configure(EntityTypeBuilder<OrderSagaState> builder)
+        {
+            builder.ToTable("OrderSagaState");
+            builder.HasKey(x => x.CorrelationId);
+
+            builder.Property(x => x.CurrentState)
+                .HasMaxLength(64)
+                .IsRequired();
+
+            builder.Property(x => x.OrderId).IsRequired();
+            builder.Property(x => x.CreatedAt).IsRequired();
+            builder.Property(x => x.UpdatedAt).IsRequired();
+            builder.Property(x => x.Amount).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.CustomerEmail).HasMaxLength(256);
+        }
+    }
+}
