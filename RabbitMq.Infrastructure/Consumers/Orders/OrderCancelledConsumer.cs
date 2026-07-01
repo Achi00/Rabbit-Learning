216de2f227d88f162b6946614bc256778e30e33b
Achi00 <toolvlg@gmail.com>
2026-07-01 22:55:54 +0400

    add simple idempotency check in one consumer

--- RabbitMq.Infrastructure/Consumers/Orders/OrderCancelledConsumer.cs
+++ RabbitMq.Infrastructure/Consumers/Orders/OrderCancelledConsumer.cs
@@ -1,20 +1,34 @@
 ﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using RabbitMq.Contracts.Events;
 using RabbitMq.Domain.Enums;
 using RabbitMQ.Application.Interfaces.Services.Orders;
+using RabbitMqDemo.Persistance.Context;
 
 namespace RabbitMq.Infrastructure.Consumers.Orders
 {
     public class OrderCancelledConsumer : IConsumer<OrderCancelled>
     {
         private readonly IOrderService _orderService;
+        private readonly MessageDbContext _context;
 
-        public OrderCancelledConsumer(IOrderService orderService)
+        public OrderCancelledConsumer(IOrderService orderService, MessageDbContext context)
         {
             _orderService = orderService;
+            _context = context;
         }
         public async Task Consume(ConsumeContext<OrderCancelled> context)
         {
+            var messageId = context.MessageId ?? Guid.NewGuid();
+
+            // idempotency check
+            var alreadyProcessed = await _context.Messages.AnyAsync(m => m.MessageId == messageId);
+
+            if (alreadyProcessed)
+            {
+                return;
+            }
+
             await _orderService.MarkCancelledAsync(context.Message.OrderId, context.Message.Reason, context.CancellationToken);
         }
     }
